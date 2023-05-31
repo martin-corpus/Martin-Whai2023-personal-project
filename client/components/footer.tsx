f1332edf247542253e045fa0f28b027ffdc53fc7
martin corpus <nasuntao@yahoo.com>
2023-06-01 07:06:46 +1200

    personal project - fix footer

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -1,9 +1,22 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
+
 export default function Footer() {
     return (
       <>
-        <div>
-            <p>WHILE YOU WORK, GROOVE YOURSELF TO EMPLOYMENT. </p>
-        </div>  
+        <div className="icons">
+          <a href="https://www.facebook.com/example" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className="icon"/>
+          </a>
+          <a href="https://www.instagram.com/example" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="icon"/>
+          </a>
+          <a href="https://www.linkedin.com/in/example" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="icon"/>
+          </a>
+          <a href="https://www.github.com/example" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="icon"/>
+          </a>
+        </div> 
       </>
     )
   }
