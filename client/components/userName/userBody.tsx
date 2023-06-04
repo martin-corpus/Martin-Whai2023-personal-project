b903548cc276fcecda66ae60f709bce6593be31c
martin corpus <nasuntao@yahoo.com>
2023-06-04 12:32:10 +1200

    personal project - user page

--- client/components/userName/userBody.tsx
+++ client/components/userName/userBody.tsx
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom'
+import { FaUser } from 'react-icons/fa'
 
 export default function UserBody() {
   return (
     <>
-      <div >
-        <p>Listen to the Secretary.</p>
-        <Link to="/">
-          <button>Go Back Home</button>
-        </Link>
-      </div>
+        <div className="userContainer">
+            <div className="userCircle">
+                <span className="userIcon">
+                <FaUser />
+                </span>
+            </div>
+            <div>
+                <h1 id="userName">Hi username!</h1>
+            </div>
+        </div>
+
+        <h3 id="applications">Applications</h3>
+        <div className="userApplications">
+
+        </div>
     </>
   )
 }
