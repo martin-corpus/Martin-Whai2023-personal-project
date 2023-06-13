05929f2c2670314fd46cb745670675e5396bbfde
martin corpus <nasuntao@yahoo.com>
2023-06-13 14:03:51 +1200

    personal project - side scroll

--- client/components/userName/userNameBody.tsx
+++ client/components/userName/userNameBody.tsx
@@ -1,10 +1,70 @@
 import { Link } from 'react-router-dom'
 import { FaUser } from 'react-icons/fa'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import  HiUserName  from '../../components/hiUserName'
 
 export default function UserNameBody() {
   
+//////////////// APPLICATIONS ///
+
+const [currentIndex, setCurrentIndex] = useState(0)
+
+
+useEffect(() => {
+    const applicationboxes = document.querySelectorAll('.applicationbox')
+    const boxesArray = Array.from(applicationboxes) as HTMLElement[]
+
+    const showCurrentLogo = () => {
+        const startIndex = currentIndex * 3
+        const endIndex = startIndex + 3
+    
+        boxesArray.forEach((box: HTMLElement, index: number) => {
+          if (index >= startIndex && index < endIndex) {
+            box.style.display = 'block'
+          } else {
+            box.style.display = 'none'
+          }
+        })
+      }
+
+      const showNextLogo = () => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex < Math.floor(boxesArray.length / 3) ? prevIndex + 1 : 0
+        )
+      }
+
+      const showPreviousLogo = () => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex > 0 ? prevIndex - 1 : Math.floor(boxesArray.length / 3)
+        )
+      }
+
+    const leftArrow = document.querySelector('.leftArrow')
+    const rightArrow = document.querySelector('.rightArrow')
+
+    if (leftArrow) {
+      leftArrow.addEventListener('click', showPreviousLogo)
+    }
+
+    if (rightArrow) {
+      rightArrow.addEventListener('click', showNextLogo)
+    }
+
+    showCurrentLogo()
+
+    return () => {
+        if (leftArrow) {
+          leftArrow.removeEventListener('click', showPreviousLogo)
+        }
+  
+        if (rightArrow) {
+          rightArrow.removeEventListener('click', showNextLogo)
+        }
+      }
+    }, [currentIndex])
+  
+
+//////////////// SEARCH ENGINE ///
 const [location, setLocation] = useState('')
 const [field, setField] = useState('')
   
@@ -30,13 +90,23 @@ return (
         <h3 id="applications">Applications</h3>
         <div className="userApplicationsContainer">
             <div className="userApplications">
-                <div className="applicationbox">Box 1</div>
-                <div className="applicationbox">Box 2</div>
-                <div className="applicationbox">Box 3</div>
-                <div className="applicationbox">Box 4</div>
-                <div className="applicationbox">Box 5</div>
-                <div className="applicationbox">Box 6</div>
-                <div className="applicationbox">Box 7</div>
+                
+                <div className="leftArrow">
+                    <i className="fa-solid fa-chevron-left"></i>
+                </div>
+
+                <div className="applicationbox" id="box1">Box 1</div>
+                <div className="applicationbox" id="box2">Box 2</div>
+                <div className="applicationbox" id="box3">Box 3</div>
+                <div className="applicationbox" id="box4">Box 4</div>
+                <div className="applicationbox" id="box5">Box 5</div>
+                <div className="applicationbox" id="box6">Box 6</div>
+                <div className="applicationbox" id="box7">Box 7</div>
+
+                <div className="rightArrow">
+                    <i className="fa-solid fa-chevron-right"></i>
+                </div>
+
             </div>
         </div>
 
