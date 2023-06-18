fc75cbc0988bf7fb294849b6a498a95956583bdf
martin corpus <nasuntao@yahoo.com>
2023-06-19 11:09:26 +1200

    personal project - dokku fix

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import { join } from 'node:path'
+import { join, resolve } from 'node:path'
 import express from 'express'
 import companies from './routes/companies'
 import vacancies from './routes/vacancies'
@@ -13,5 +13,14 @@ server.use('/api/v1', companies)
 server.use('/api/v1', vacancies)
 server.use('/api/v1', users)
 server.use('/api/v1', applications)
+
+
+if (process.env.NODE_ENV === 'production') {
+    server.use('/assets', express.static(resolve(__dirname, '../assets')))
+    server.get('*', (req, res) => {
+      res.sendFile(resolve(__dirname, '../index.html'))
+    })
+}
+
 
 export default server
