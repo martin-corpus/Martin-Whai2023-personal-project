7b1e775710231a04624239747ac6fc7033451b7c
martin corpus <nasuntao@yahoo.com>
2023-07-13 19:13:52 +1200

    personal project - make 1st test for app component

--- client/components/__tests__/App.test.tsx
+++ client/components/__tests__/App.test.tsx
@@ -1,40 +1,20 @@
 // @vitest-environment jsdom
 import { describe, expect, it, vi } from 'vitest'
 import { renderRoute } from '../../test/utils'
-import readFile from '../../apis/getRandomQuote'
 import { screen, waitFor } from '@testing-library/react'
 
-vi.mock('../../apis/getRandomQuote')
-
-describe('<Navigation>', () => {
-  it('renders navigation links', async () => {
-    vi.mocked(readFile).mockResolvedValue({
-      quote:
-        '`Music is a therapy. It is a communication far more powerful than words, far more immediate, far more efficient.` - Yehudi Menuhin.',
-    })
-
+describe('User home page', () => {
+  it('should have company slogan', async () => {
     renderRoute('/')
     await waitFor(() => {
-      expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
+      expect(
+        screen.queryByText(/WHILE YOU WORK, GROOVE YOURSELF TO EMPLOYMENT./i)
+      ).toBeInTheDocument()
     })
-
-    const links = await screen.findAllByRole('link')
-
-    links.forEach((link) => {
-      expect(link).toBeInTheDocument()
-    })
-
-    expect(links.map((link) => link.textContent)).toMatchInlineSnapshot(`
-      [
-        "Play Piano",
-        "Become A Singer",
-        "Playground",
-      ]
-    `)
   })
 })
 
-it('navigates to the correct routes when clicked', async () => {
+it.todo('navigates to the correct routes when clicked', async () => {
   renderRoute('/')
   await waitFor(() => {
     expect(screen.queryByText(/loading/i)).not.toBeInTheDocument()
